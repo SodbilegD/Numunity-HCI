66439ea40309c9e415fb7e9059f6dd3a1aabedd3
sodbilegd <21b1num0742@stud.num.edu.mn>
2023-11-16 15:12:42 +0800

    asdf

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,12 +2,13 @@ import '../styles/styles.css';
 import 'lazysizes';
 import './modules/chat.js';
 import './modules/sidebar.js';
+// import './modules/pagination.js'
 import './modules/dataFetcher.js';
 // import './modules/ulrParser.js';
 // import './modules/selected-main.js';
 //import './modules/post.json';
 import './modules/post.js';
-import './modules/recentPost.js';
+// import './modules/recentPost.js';
 if(module.hot) {
     module.hot.accept();
 }
