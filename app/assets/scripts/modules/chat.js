5e0669cbf5be916ebb386448189731cebbf4ca9e
Khulan Battur <117669774+kulnaak@users.noreply.github.com>
2023-12-06 18:59:51 +0800

    khuln

--- app/assets/scripts/modules/chat.js
+++
@@ -1,9 +0,0 @@
-var userWindow = document.querySelector(".users-window");
-function show_hide(){
-	if(userWindow.style.display == "none"){
-		userWindow.style.display = "block";
-	}
-	else{
-		userWindow.style.display = "none";
-	}
-}
