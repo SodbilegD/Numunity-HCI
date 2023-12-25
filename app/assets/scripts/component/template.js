183c0169517c2c4f7daabd97da2a83115ec8ab14
Dulguun Baldorj <duk.dulguun46@gmail.com>
2023-12-25 17:57:42 +0800

    yt

--- app/assets/scripts/component/template.js
+++ app/assets/scripts/component/template.js
@@ -2,7 +2,7 @@ const { template } = require("lodash");
 customElements.define('post-template',
   class extends HTMLElement {
     constructor() {
-      super(); // Always call super first in constructor
+      super(); // Alway scall super first in constructor
       const postTemplate = document.getElementById('post-template').content;
       const shadowRoot = this.attachShadow({
         mode: 'open'
@@ -19,82 +19,85 @@ customElements.define('post-template',
   <style>
   @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap');
   
-  .post{
+  .post {
     padding: 1rem 2rem;
     margin-bottom: 1.5rem;
     background-color: #ffffff;
     border-radius: 0.5rem;
     border: 1px solid #5555d8;
+}
 
-    .post__profile{
-        display: flex;
-        direction: row;
-        align-items: center;
-        margin: 0;
-        
-        .post__profile__img{
-            width: 2.5rem;
-            padding-right: 1.5rem;
-        }
-        .post__profile__name{
-            font-size: 1.25rem;
-        }
-        .post__profile__time{
-            margin-left: auto;
-            opacity: 0.5;
-        }
-        .post__profile__community{
-            margin-left: auto;
-            opacity: 0.5;
-            font-size: 1rem;
-            text-decoration: none;
-            color: #000000;
-        }
-    }
+.post__profile {
+    display: flex;
+    direction: row;
+    align-items: center;
+    margin: 0;
+}
 
-    hr{
-        opacity: 0.5;
-        margin: 0;
-    }
+.post__profile__img {
+    width: 2.5rem;
+    padding-right: 1.5rem;
+}
 
-    .post__title{
-        font-size: 1.25rem;
-        font-weight: bold;
-        cursor: pointer;
-        transition: transform 0.2s ease-in-out;
-    }
+.post__profile__name {
+    font-size: 1.25rem;
+}
 
-    .post__title:hover {
-        transform: translateX(0.5rem);
-        color: #5555d8;
-    }
-    .post__detail{
-        font-size: 1rem;
-        text-align: justify;
-    }
-    .post__reactions{
-        display: flex;
-        direction: row;
-        justify-content: flex-start;
+.post__profile__time {
+    margin-left: auto;
+    opacity: 0.5;
+}
 
-        .post__list{
-            padding-right: 2.5rem;
-            .clicked {
-                background-color: #ffffff;
-                color: #5555d8;
-                .reaction-button{
-                    background-color: #ffffff;
-                    color: #5555d8;
-                }
-            }
-            .reaction-button{
-                border: none;
-                background-color: #ffffff;
-                color: #000000;
-              }
-          }
-        }
-      }
+.post__profile__community {
+    margin-left: auto;
+    opacity: 0.5;
+    font-size: 1rem;
+    text-decoration: none;
+    color: #000000;
+}
+
+hr {
+    opacity: 0.5;
+    margin: 0;
+}
+
+.post__title {
+    font-size: 1.25rem;
+    font-weight: bold;
+    cursor: pointer;
+    transition: transform 0.2s ease-in-out;
+}
+
+.post__title:hover {
+    transform: translateX(0.5rem);
+    color: #5555d8;
+}
+
+.post__detail {
+    font-size: 1rem;
+    text-align: justify;
+}
+
+.post__reactions {
+    display: flex;
+    direction: row;
+    justify-content: flex-start;
+}
+
+.post__list {
+    padding-right: 2.5rem;
+}
+
+.post__list .clicked {
+    background-color: #ffffff;
+    color: #5555d8;
+}
+
+.post__list .reaction-button {
+    border: none;
+    background-color: #ffffff;
+    color: #000000;
+}
    </style>
         <fieldset>
             <slot name="post-template-legend">
