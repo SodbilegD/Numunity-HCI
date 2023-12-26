243043844974f77a7405fd9bffa1229e5926ed24
Khulan Battur <117669774+kulnaak@users.noreply.github.com>
2023-12-26 10:28:15 +0800

    k

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,8 +3,6 @@ import 'lazysizes';
 import './modules/chat.js';
 import './modules/sidebar.js';
 import './modules/dataFetcher.js';
-// import './modules/filter.js';
-import './session_ram/app.mjs';
 if(module.hot) {
     module.hot.accept();
 }
