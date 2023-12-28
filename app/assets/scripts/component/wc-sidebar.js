787a217a7d9f9aa5a9311c1db688652881c37c30
SodbilegD <21b1num0742@stud.num.edu.mn>
2023-12-28 14:23:52 +0800

    qwertyui

--- app/assets/scripts/component/wc-sidebar.js
+++ app/assets/scripts/component/wc-sidebar.js
@@ -23,15 +23,37 @@ class WcSidebar extends HTMLElement {
             </ul>
         </nav>
         `;
+        
+        const toggle = document.querySelector(".toggle");
+        const slide = document.querySelector(".slide");
+        const slideTitle = document.querySelector(".slide__title");
+        const slideList = document.querySelector(".slide__list");
+        const slideItems = document.querySelectorAll(".slide__list__item");
 
-        document.addEventListener("toggleSidebar", () => {
-            this.toggleSidebar();
-        })
-    }
+        let showMenu = false;
+        toggle.addEventListener("click", () => {
+            if (!showMenu) {
+                toggle.classList.add("close");
+                slide.classList.add("show");
+                slideTitle.classList.add("show");
+                slideList.classList.add("show");
+                slideItems.forEach((item) =>
+                    item.classList.add("show"));
 
-    toggleSidebar() {
-        const slide = this.querySelector(".slide");
-        slide.classList.toggle("show");
+                // Reset the menu state
+                showMenu = true;
+            } else {
+                toggle.classList.remove("close");
+                slide.classList.remove("show");
+                slideTitle.classList.remove("show");
+                slideList.classList.remove("show");
+                slideItems.forEach((item) =>
+                    item.classList.remove("show"));
+
+                // Reset the menu state
+                showMenu = false;
+            }
+        });
     }
 
     disconnectedCallback() {
