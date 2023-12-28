426572d762cf3465fb3207c878bd3886ec2ff347
Dulguun Baldorj <duk.dulguun46@gmail.com>
2023-12-28 20:04:49 +0800

    temp

--- app/assets/scripts/component/wc-community-section.js
+++ app/assets/scripts/component/wc-community-section.js
@@ -1,161 +1,220 @@
 // import { fetchData } from "../modules/dataFetcher.js";
-//advertisements deer bairlah ali ng community medeelliig haruulah hsg
-class theCommunitySection extends HTMLElement {
+// import { sendDataToJsonBin } from "../modules/dataFetcher.js";
+
+class PostComment extends HTMLElement {
     constructor() {
         super();
         this.postsContainer = document.getElementById("posts-container");
-        this.trendButtonElement = document.getElementById("trendButton");
-        this.newButtonElement = document.getElementById("newButton");
-        this.communityId = null;
+        this.commentsContainer = document.getElementById("comments-container");
+        this.commentCounter = document.getElementById("total-comments");
+    
+        this.trendCommentButton = document.getElementById("comment-trend-filter");
+        this.trendCommentButton.addEventListener("click", this.filterTrend.bind(this));
 
-        this.trendButtonElement.addEventListener("click", this.filterTrend.bind(this));
-        this.newButtonElement.addEventListener("click", this.filterNew.bind(this));
-        
-        this.darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    }
-
-    #render(community) {
-        document.getElementById("community").insertAdjacentHTML('afterbegin', `
-            <h1 class="community__name">>> ${community.communityName}</h1>`);
-        document.getElementById("community-detail").insertAdjacentHTML('afterbegin', `
-            <h3 class="advertisements__info__title">${community.communityName}</h3>
-            <hr>
-
-            
-            
-            <p class="advertisements__info__detail">${community.communityAbout}</p>
-            <p class="advertisements__info__opened"><i class="fa-solid fa-clock"></i>Нээгдсэн: ${community.createdDate}</p>
-            <div class="advertisements__info__container">
-                <p class="advertisements__info__followers">Дагагчид<br><span>${community.followers.length}</span></p>
-                <hr>
-                <p class="advertisements__info__total">Нийт пост<br><span>${community.posts.length}</span></p>
-            </div>
-        `);
-        this.posts = community.posts;
-        this.renderPosts(this.posts);
+        const sendCommentButton = document.getElementById("send-comment-button");
+        sendCommentButton.addEventListener("click", this.handleSendComment.bind(this));
     }
 
     async connectedCallback() {
         try {
             const currentUrl = new URL(window.location.href);
             this.communityId = currentUrl.searchParams.get('communityId');
-            const response = await fetch(`http://localhost:3000/selectedcommunity`, {
+            this.postId = currentUrl.searchParams.get('postId');
+            const response = await fetch(`http://localhost:3000/discussion`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json; charset=UTF-8',
                 },
-                body: JSON.stringify({ communityId: this.communityId }),
+                body: JSON.stringify({ communityId: this.communityId , postId: this.postId }),
             });
-            console.log("responsee",response);
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
 
             const data = await response.json();
+            
             this.users = data.user;
-            this.#render(data.community);
+            this.community = data.community;
+            this.post = this.community.posts.find(e => e.postId === this.postId);
+            this.comments = this.post.comments;
+            this.sessions = data.sessions;
+
+            this.renderSinglePost(this.post);
+            this.renderComments(this.comments);
 
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     }
 
-    filterTrend() {
-        const currentDate = new Date();
-        const filteredTrend = this.posts.filter(post =>
-            Date.parse(post.publishedDate) > currentDate - 7 && post.agreeCount > 15
-        );
-    
-        this.renderPosts(filteredTrend);
-    }
-    
-    filterNew() {
-        const currentDate = new Date();
-        const filteredNew = this.posts.filter(post =>
-        Date.parse(post.publishedDate) > currentDate - 7);
-    
-        this.renderPosts(filteredNew);
+    // async connectedCallback() {
+    //     await this.fetchData();
+    //     this.renderSinglePost(this.post);
+    //     this.renderComments(this.comments);
+    // }
+
+    // async fetchData() {
+    //     this.jsondata = await fetchData();
+    //     const currentUrl = new URL(window.location.href);
+    //     const communityId = currentUrl.searchParams.get('communityId');
+    //     const postsId = currentUrl.searchParams.get('postId');
+        
+    //     this.community = this.jsondata.record.community[communityId - 1];
+    //     console.log(this.community);
+    //     this.posts = this.community.posts;
+    //     this.post = this.posts.find(post => post.postId == postsId);
+    //     this.comments = this.post.comments || [];
+    // }
+
+    renderComments(comments) {
+        this.commentsContainer.innerHTML = "";
+        comments.forEach(comment => { 
+            const userId = comment.user;
+            const user = this.users.find(user => user.userId === parseInt(userId));
+            this.#render(comment, user)
+        });
+        this.commentCounter.innerHTML = this.comments.length;
     }
 
-    renderPosts(posts) {
-        this.postsContainer.innerHTML = "";
-        posts.forEach(post => {
-            const userId = post.user;
-            const user = this.users.find(user => user.userId === parseInt(userId));
-            this.#RenderPost(post, user);
-        });
+    #render(comment, user){
+        this.commentPublishedDate = comment.publishedDate;
+        
+        var commentElement = document.createElement('div');
+        commentElement.classList.add('single-comment');
+        commentElement.id = `single-comment_${(comment.commentId)}`;
+
+        commentElement.insertAdjacentHTML("afterbegin",
+        `<div class="single-comment__profile">
+                <img src="${user.profImg}" alt="profile" class="single-comment__profile__img">
+                <p class="single-comment__profile__name">${user.userName}</p>
+            </div>
+            <p class="single-comment__detail">${comment.body}</p>
+
+            <div class="single-comment__reactions">
+                <agree-disagree agreeCount=${comment.agreeCount} disagreeCount=${comment.disagreeCount} isAgreeClicked=${false} isDisAgreeClicked=${false}></agree-disagree>                                                           
+                <p class="single-comment__reactions__list"><i class="fa-solid fa-reply"></i>Reply</p>
+            </div>`);
+        this.commentsContainer.appendChild(commentElement);        
     }
-    
-    #RenderPost(post, user) {       
-        
-        var postElement = document.createElement('article');
-        postElement.classList.add('post');
-        postElement.id = `recentPost_${post.postId}`;
-        postElement.insertAdjacentHTML("afterbegin", `
-        
-        // testing some dark mode thing
-        theme="${window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"}">
-        
-        
-        
-        <div class="post__profile">
+
+    renderSinglePost(post) {
+        const userId = post.user;
+        const user = this.users.find(user => user.userId === parseInt(userId));
+
+        this.postsContainer.innerHTML = `
+        <article class="post" id="recentPost_${post.postId}">
+            <div class="post__profile" id="posts-container">
                 <img src="${user.profImg}" alt="profile" class="post__profile__img">
                 <p class="post__profile__name">${user.userName}</p>
-                <p class="post__profile__time">${post.publishedDate}</p>
+                <a href="selectedcommunity.html?communityId=${this.community.communityId}" class="post__profile__community">>>${this.community.communityName}</a>
             </div>
-            <hr>`);
-            var postTitleElement = document.createElement('h3');
-            postElement.appendChild(postTitleElement);
-            postTitleElement.classList.add('post__title');
-            postTitleElement.textContent = post.postTitle;
-
-            
-            const postTemplate = document.getElementById('post-template');
-            const clone = document.importNode(postTemplate.content, true);
-            clone.querySelector('[slot="userName"]').textContent = user.userName;
-            clone.querySelector('[slot="publishedDate"]').textContent = post.publishedDate;
-            clone.querySelector('[slot="postTitle"]').textContent = post.postTitle;
-            clone.querySelector('[slot="postDetail"]').textContent = post.postDetail;
-            clone.querySelector('[slot="commentCount"]').textContent = post.comments.length;
-            clone.querySelector('[slot="shareCount"]').textContent = post.shareCount;
-            this.postsContainer.appendChild(clone);
-    
-
-            postElement.insertAdjacentHTML("beforeend",`
-            
-            <template id="post-template">
-            <article class="post">
-              <div class="post__profile">
-                <img class="post__profile__img" alt="profile" src="" />
-                <p class="post__profile__name"><slot name="userName"></slot></p>
-                <p class="post__profile__time"><slot name="publishedDate"></slot></p>
-              </div>
-              <hr />
-              <h3 class="post__title"><slot name="postTitle"></slot></h3>
-
-            <p class="post__detail"><slot name="postDetail">${post.postDetail}</slot></p>
-            <div class="post__reactions">
-                <agree-disagree agreeCount=${post.agreeCount} disagreeCount=${post.disagreeCount} isAgreeClicked=${false} isDisAgreeClicked=${false}></agree-disagree>
+            <hr>
+            <h1 class="post__title">${post.postTitle}</h1>
+            <p class="post__detail">${post.postDetail}</p>
+            <div class="post__reactions post__reactions--hidden">
+                <agree-disagree agreeCount="${post.agreeCount}" disagreeCount="${post.disagreeCount}"></agree-disagree>
                 <p class="post__reactions__list">
                     <i class="fa-regular fa-comment post__reactions__icon"></i>
                     <span class="reaction-count">${post.comments.length}</span> Comment
                 </p>
                 <p class="post__reactions__list">
                     <i class="fa-regular fa-share-from-square post__reactions__icon"></i>
-                    <span class="reaction-count"><slot name="shareCount">${post.shareCount}</slot></span> Share
+                    <span class="reaction-count">${post.shareCount}</span> Share
                 </p>
             </div>
-        </article>
-        </template>`);
-        this.postsContainer.appendChild(postElement);
-        this.addEventListenerToPostTitle(postTitleElement, post.postId);
+            <p class="post__profile__time post__profile__time--down">1h ago</p>
+        </article>`;
     }
 
-    addEventListenerToPostTitle(postElement, postId) {
-        postElement.addEventListener("click", () => {
-            window.location.href = `discussion.html?communityId=${this.communityId}&postId=${postId}`;
-        });
-    };
+    filterTrend() {
+        const currentDate = new Date();
+        const filteredTrend = this.comments.filter(comment =>
+        Date.parse(comment.publishedDate) > currentDate - 7 && comment.agreeCount > 10
+        );
+
+        this.renderComments(filteredTrend);
+    }
+
+    async handleSendComment() {
+        const currentUrl = new URL(window.location.href);
+        const communityId = currentUrl.searchParams.get('communityId');
+        const postId = currentUrl.searchParams.get('postId');
+        const commentCounter = document.getElementById("total-comments");
+        const commentInput = document.getElementById("comment-input");
+        console.log(commentInput.value);
+        const newCommentText = commentInput.value.trim();
+        
+        if (newCommentText !== "") {
+            
+            const response = await fetch("http://localhost:3000/getuser",
+            {
+                method: 'POST',
+                cache: "no-cache",
+                headers: {
+                    "Content-Type": 'application/json; charset=UTF-8'
+                }
+            });
+            console.log("responseeeee",response);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            const user = data.user;
+
+            const newComment = {
+                id: commentCounter.innerHTML,
+                body: newCommentText,
+                user: user.userId,
+                publishedDate: new Date().toISOString(),
+                agreeCount: 0,
+                disagreeCount: 0
+            };
+
+            const otherResponse = await fetch("http://localhost:3000/addnewcomment",
+            {
+                method: 'POST',
+                cache: "no-cache",
+                headers: {
+                    "Content-Type": 'application/json; charset=UTF-8'
+                },
+                body: JSON.stringify({ communityId: communityId, postId: postId, newComment: newComment})
+            });
+            if (!otherResponse.ok) {
+                throw new Error(`HTTP error! Status: ${otherResponse.status}`);
+            }
+            const otherdata = await otherResponse.json();
+            this.comments = otherdata.comments;
+            this.renderComments(this.comments);
+            commentInput.value = "";
+        };}
+
+    
+    // async handleSendComment() {
+    //     const commentInput = document.getElementById("comment-input");
+    //     const newCommentText = commentInput.value.trim();
+    
+    //     if (newCommentText !== "") {
+    //         const newComment = {
+    //             id: this.commentCounter.innerHTML,
+    //             body: newCommentText,
+    //             user: {
+    //                 id: 999,
+    //                 username: "comment writer",
+    //                 profileImage: "/assets/images/profile.png"
+    //             },
+    //             publishedDate: new Date().toISOString(),
+    //         };
+    
+    //         this.comments.push(newComment);
+    
+    //         this.renderComments(this.comments);
+    
+    //         commentInput.value = "";
+    //         await sendDataToJsonBin(this.jsondata.record);
+    //     }
+    // }
+    
 
     disconnectedCallback() {
         //implementation
@@ -164,12 +223,11 @@ class theCommunitySection extends HTMLElement {
     attributeChangedCallback(name, oldVal, newVal) {
         //implementation
     }
-    
+
     adoptedCallback() {
         //implementation
     }
 
 }
 
-window.customElements.define('the-community-section', theCommunitySection);
-export const community = new theCommunitySection();
+window.customElements.define('post-comment', PostComment);
